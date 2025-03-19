1210a151ad87bf6935d74279173762472c4103dd
Anders-Hedegaard <anhed23@student.sdu.dk>
2025-03-19 10:46:13 +0100

    Added files

---
+++ Asteroids/src/main/java/dk/sdu/cbse/asteroid/AsteroidSplitterImpl.java
@@ -0,0 +1,17 @@
+package dk.sdu.cbse.asteroid;
+
+import dk.sdu.cbse.common.asteroids.IAsteroidSplitter;
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.World;
+
+/**
+ *
+ * @author corfixen
+ */
+public class AsteroidSplitterImpl implements IAsteroidSplitter {
+
+    @Override
+    public void createSplitAsteroid(Entity e, World world) {
+    }
+
+}
